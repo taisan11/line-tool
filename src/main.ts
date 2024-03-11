761c62bfd8f86b0f4e4aa13a09ac5703a2c5ed67
taisan <131413638+taisan11@users.noreply.github.com>
2024-03-11 14:00:51 +0900

    hoi

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import './style.css';
 import liff from '@line/liff';
+import LIFFInspectorPlugin from '@line/liff-inspector';
 
 const app = document.querySelector<HTMLDivElement>('#app') as HTMLDivElement;
 
 // 読み込み時に実行
 window.onload = async () => {
+  const url = new URL(window.location.href);
+  const param = url.searchParams
+  if (param.get('debug') === 'true') {
+    console.log('Debug Mode!!!:', param.get('liff.state'));
+    liff.use(new LIFFInspectorPlugin());
+  }
   // LIFFの初期化
   await liff.init({liffId: import.meta.env.VITE_LIFF_ID});
   // ログインしていない場合はログイン画面にリダイレクト
